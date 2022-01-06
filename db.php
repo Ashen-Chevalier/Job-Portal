fbe0f41fe85053e745de5e0497a446cf822e86d3
Ashen-Chevalier <mayankshandilya1@gmail.com>
2022-01-07 00:37:55 +0530

    connected to database

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""
+$database = "jobs"
+
+$conn=new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+if (($handle = fopen("monster.csv", "r")) !== FALSE){
+    while(($row = fgetcsv($handle)) !== FALSE){
+        $db -> query('INSERT INTO jobs (posts, companys, place, experience) VALUES ("'.$row[0].'", "'.$row[1].'", "'.$row[2]'", "'.$row[3].'")');
+
+    }
+    fclose($handle);
+}
+$conn -> close();
+
+?>
